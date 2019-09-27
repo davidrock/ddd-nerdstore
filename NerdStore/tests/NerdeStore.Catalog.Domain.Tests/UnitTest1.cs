a703d9d875798b9e406c284ccd947497979667ba
david.almeida <davidrocha@outlook.com>
2019-09-27 20:02:34 -0300

    Create Test folder and add the first test

---
+++ NerdStore/tests/NerdeStore.Catalog.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace NerdeStore.Catalog.Domain.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void Product_Validate_ValidationsReturnExceptions()
+        {
+            //Arrange
+
+        }
+    }
+}
